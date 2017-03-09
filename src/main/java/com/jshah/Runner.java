d72c42e1c93a5fab466d1fa3346ced46b8470845
Jay Shah <contactjay@gmail.com>
2017-03-08 22:50:47 -0800

    initial commit

---
+++ src/main/java/com/jshah/Runner.java
@@ -0,0 +1,16 @@
+package com.jshah;
+
+import org.apache.http.HttpRequest;
+import org.apache.http.client.*;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+/**
+ * Created by jay.shah on 3/8/17.
+ */
+
+public class Runner {
+    public static void main(String[] args) {
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpRequest
+    }
+}
