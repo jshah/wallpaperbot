7ae6c484cc030d18b42beffa9d6736829621fe68
Jay Shah <contactjay@gmail.com>
2017-03-10 20:14:15 -0800

    returning top 10 posts from week in /r/wallpapers

--- src/main/java/com/jshah/wallpaperbot/Wallpaperbot.java
+++ src/main/java/com/jshah/wallpaperbot/Wallpaperbot.java
@@ -5,13 +5,16 @@ import net.dean.jraw.RedditClient;
 import net.dean.jraw.http.UserAgent;
 import net.dean.jraw.http.oauth.Credentials;
 import net.dean.jraw.http.oauth.OAuthData;
-import net.dean.jraw.http.oauth.OAuthHelper;
+import net.dean.jraw.models.Listing;
+import net.dean.jraw.models.Submission;
+import net.dean.jraw.paginators.Sorting;
+import net.dean.jraw.paginators.SubredditPaginator;
+import net.dean.jraw.paginators.TimePeriod;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
-import java.util.UUID;
 
 /**
  * Created by jay.shah on 3/9/17.
@@ -22,37 +25,44 @@ public class Wallpaperbot {
     private InputStream inputStream = null;
 
     public void run() {
+        // read from config.properties
         loadProperties();
-
         String password = properties.getProperty("password");
         String secret = properties.getProperty("secret");
-        UUID uuid = UUID.randomUUID();
 
         UserAgent userAgent = UserAgent.of(AppResources.platform, AppResources.clientID, AppResources.version, AppResources.username);
         RedditClient reddit = new RedditClient(userAgent);
         Credentials credentials = Credentials.script(AppResources.username, password, AppResources.clientID, secret);
+        authenticate(reddit, credentials);
 
-        OAuthData oAuthData = easyAuth(reddit.getOAuthHelper(), credentials);
-        if (oAuthData != null) {
-            try {
-                reddit.authenticate(oAuthData);
-                reddit.me();
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
-        else {
-            System.out.println("Authentication Failed");
-        }
+        // find top wallpapers
+        wallpapersPaginator(reddit);
     }
 
-    private OAuthData easyAuth(OAuthHelper oAuthHelper , Credentials credentials) {
-        try {
-            return oAuthHelper.easyAuth(credentials);
-        } catch (Exception e) {
-            e.printStackTrace();
+    private void wallpapersPaginator(RedditClient reddit) {
+        SubredditPaginator paginator = new SubredditPaginator(reddit);
+        paginator.setLimit(10);
+        paginator.setSorting(Sorting.TOP);
+        paginator.setTimePeriod(TimePeriod.WEEK);
+        paginator.setSubreddit("wallpapers");
+
+        Listing<Submission> listing = paginator.next(true);
+        while(!listing.isEmpty()) {
+            String title = listing.get(0).getTitle();
+            String url = listing.get(0).getUrl();
+            System.out.println(title + " : " + url);
+            listing = paginator.next(true);
         }
-        return null;
+
+    }
+
+    private void authenticate(RedditClient reddit, Credentials credentials) {
+        try {
+            OAuthData oAuthData = reddit.getOAuthHelper().easyAuth(credentials);
+            reddit.authenticate(oAuthData);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     private void loadProperties() {
