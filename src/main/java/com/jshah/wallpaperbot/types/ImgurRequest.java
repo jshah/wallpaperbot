ca0bc7c45f2b82f84dab80bfd429667adbec75e1
jay.shah <jay.shah@workday.com>
2017-03-12 22:41:21 -0700

    adding initial structure for imgur support

---
+++ src/main/java/com/jshah/wallpaperbot/types/ImgurRequest.java
@@ -0,0 +1,14 @@
+package com.jshah.wallpaperbot.types;
+
+/**
+ * Created by jay.shah on 3/12/17.
+ */
+
+public class ImgurRequest {
+    private String authorizationUrl = "https://api.imgur.com/oauth2/authorize";
+
+    public ImgurRequest() {}
+
+    public void authorizeToken() {
+    }
+}
