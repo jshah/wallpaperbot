00d1d3010ac9d0c2a871c25e3937019e2167b4d7
jay.shah <jay.shah@workday.com>
2017-03-16 00:52:58 -0700

    clean up

--- src/main/java/com/jshah/wallpaperbot/types/ImgurRequest.java
+++ src/main/java/com/jshah/wallpaperbot/types/ImgurRequest.java
@@ -1,12 +1,25 @@
 package com.jshah.wallpaperbot.types;
 
 import com.jshah.wallpaperbot.resources.ConfigHandler;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
 
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -20,23 +33,16 @@ public class ImgurRequest extends Request implements ImageHandler {
         ConfigHandler configHandler = new ConfigHandler();
         Properties properties = configHandler.loadProperties();
         String clientID = properties.getProperty("imgurID");
-        HttpClient client = HttpClientBuilder.create().build();
+        String secret = properties.getProperty("imgurSecret");
         try {
-            URIBuilder builder = new URIBuilder(apiUrl);
-            builder.addParameter("response_type", "code");
-            builder.addParameter("client_id", clientID);
-            HttpGet request = new HttpGet(builder.build());
-            HttpResponse response = client.execute(request);
-
-            System.out.println(response.getEntity());
-            System.out.println(response.getStatusLine());
+            // TODO: need to implement oauth authorization
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public void setupDownload(String url) {
-        authenticate();
+//        authenticate();
     }
 
     public void executeDownload() {
