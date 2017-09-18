ddd1bb432175c29997edafb1835eb19c1287d939
jay.shah <jay.shah@workday.com>
2017-09-17 22:48:59 -0700

    trying out java mail api

--- src/main/java/com/jshah/wallpaperbot/external/Email.java
+++ src/main/java/com/jshah/wallpaperbot/external/Email.java
@@ -1,11 +1,73 @@
 package com.jshah.wallpaperbot.external;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.util.Properties;
+
 /**
  * Created by jay.shah on 9/17/17.
  */
 
 public class Email {
-    public static void sendMail(String filename) {
+    final static String username = "contactjay@gmail.com";
+    final static String password = "*****";
 
+    public static void sendMail(String filename) {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", true);
+        props.put("mail.smtp.starttls.enable", true);
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+            Transport transport = session.getTransport();
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(username));
+            message.setSubject("Testing Subject");
+            message.setText("PFA");
+
+            MimeBodyPart messageBodyPart = new MimeBodyPart();
+
+            Multipart multipart = new MimeMultipart();
+
+            messageBodyPart = new MimeBodyPart();
+            DataSource source = new FileDataSource(filename);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(filename);
+            multipart.addBodyPart(messageBodyPart);
+
+            message.setContent(multipart);
+
+            System.out.println("Sending");
+
+            transport.connect();
+            Transport.send(message);
+            transport.close();
+
+            System.out.println("Done");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
+
