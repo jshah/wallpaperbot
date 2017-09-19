5ddec57748d088ba3fc945f41497c4993a3fed04
Jay Shah <contactjay@gmail.com>
2017-09-18 23:23:08 -0700

    attempting to get web server up and running

--- src/main/java/com/jshah/Runner.java
+++ src/main/java/com/jshah/Runner.java
@@ -1,6 +1,7 @@
 package com.jshah;
 
 import com.jshah.wallpaperbot.Wallpaperbot;
+import com.jshah.web.JettyWebServer;
 
 /**
  * Created by jay.shah on 3/8/17.
@@ -9,6 +10,13 @@ import com.jshah.wallpaperbot.Wallpaperbot;
 public class Runner {
     public static void main(String[] args) {
         Wallpaperbot wallpaperbot = new Wallpaperbot();
+        JettyWebServer webServer = new JettyWebServer();
+        try {
+            webServer.setupWebServer();
+        } catch (Exception e) {
+            System.out.println("Server failed to start");
+            e.printStackTrace();
+        }
         wallpaperbot.run();
     }
 }
