cc0fcd21040a4ec3c1fc737f18b3af29cb389eba
jay.shah <jay.shah@workday.com>
2017-09-21 12:15:44 -0700

    getting mongodb up and running

--- src/main/java/com/jshah/db/MongoDB.java
+++ src/main/java/com/jshah/db/MongoDB.java
@@ -1,7 +1,10 @@
 package com.jshah.db;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
@@ -14,14 +17,15 @@ public class MongoDB {
         MongoClient client = new MongoClient("127.0.0.1", 27017);
         MongoDatabase database = client.getDatabase("wallpaperbot");
         MongoCollection<Document> collection = database.getCollection("users");
+        MongoCursor<Document> cursor = collection.find().iterator();
 
-        Document doc = new Document("name", "Jay Shah")
-                .append("type", "user")
-                .append("size", "1920x1080");
-        collection.insertOne(doc);
-        System.out.println(collection.count());
+        try {
+            while (cursor.hasNext()) {
+                Document doc = cursor.next();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        Document myDoc = collection.find().first();
-        System.out.println(myDoc.toJson());
     }
 }
